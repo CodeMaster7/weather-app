8fc412626c95a02b508bbf2c2e56a3069c2a0611
sam <sammybonfanti@gmail.com>
2020-10-07 22:38:24 -0600

    weather icon & images

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,42 @@
 // DOM manipulation and event handling
 const cityForm = document.querySelector('form')
+const card = document.querySelector('.card')
+const details = document.querySelector('.details')
+const time = document.querySelector('img.time')
+const icon = document.querySelector('.icon img')
+
+// Update the UI with the data
+const updateUI = (data) => {
+    // const cityDets = data.cityDets
+    // const weather = data.weather
+
+    // destructure properties - same as above
+    const {cityDets, weather} = data // from this data object i want you to get the cityDets property and store it in a const named cityDets
+
+    // update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Imperial.Value}</span>
+            <span>&deg;F</span>
+        </div>
+    `
+
+    // update the night/day & icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+    icon.setAttribute('src', iconSrc)
+
+    // ternary operator
+    let timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg'
+    time.setAttribute('src', timeSrc)
+
+    // remove the d-none class if present
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none')
+    }
+}
+
 const updateCity = async (city) => {
     const cityDets = await getCity(city) // wait before the promise finishes then assign it to the variable
     const weather = await getWeather(cityDets.Key)
@@ -18,6 +55,6 @@ cityForm.addEventListener('submit', (e) => {
 
     // update the ui with new city
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(err => console.log(err))
 })
