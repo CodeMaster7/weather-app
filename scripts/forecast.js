3add6ba7ef7fc76726e6c57a8be9b4e86b39dbf4
sam <sammybonfanti@gmail.com>
2020-10-07 22:46:48 -0600

    removed the key from the file

---
+++ scripts/forecast.js
@@ -0,0 +1,21 @@
+// get weather information
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${keys}`
+
+    const response = await fetch(base + query)
+    const data = await response.json()
+
+    return data[0]
+}
+
+// city information
+const getCity = async (city) => {
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+    const query = `?apikey=${keys}&q=${city}`
+
+    const response = await fetch(base + query)
+    const data = await response.json()
+
+    return data[0]
+}
