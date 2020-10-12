2a2210a1feb5b38df37d84bb3a0eb606c62e582f
sam <sammybonfanti@gmail.com>
2020-10-11 22:25:00 -0600

    changed the forecast into a class

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,23 +1,30 @@
-const keys = 'AxVSy66vGIMAT6FkCGmpkY6zv6HfhjGO'
+class Forecast {
+    constructor() {
+        this.key = 'MTZQWIXLHujCjSBy7G9doqAIiYyWCgbH'
+        this.weatherUrI = 'https://dataservice.accuweather.com/currentconditions/v1/'
+        this.cityURI = 'https://dataservice.accuweather.com/locations/v1/cities/search'
+    }
+    async updateCity (city) {
+        const cityDets = await this.getCity(city) // wait before the promise finishes then assign it to the variable
+        const weather = await this.getWeather(cityDets.Key)
 
-// get weather information
-const getWeather = async (id) => {
-    const base = 'https://dataservice.accuweather.com/currentconditions/v1/'
-    const query = `${id}?apikey=${keys}`
+        // Object Shorthand Notaion = when the property or key is the same as the value
+        return { cityDets,weather }
+    }
 
-    const response = await fetch(base + query)
-    const data = await response.json()
+    async getWeather (id) {
+        const query = `${id}?apikey=${this.key}`
+        const response = await fetch(this.weatherUrI + query)
+        const data = await response.json()
 
-    return data[0]
-}
+        return data[0]
+    }
 
-// city information
-const getCity = async (city) => {
-    const base = 'https://dataservice.accuweather.com/locations/v1/cities/search'
-    const query = `?apikey=${keys}&q=${city}`
+    async getCity (city) {
+        const query = `?apikey=${this.key}&q=${city}`
+        const response = await fetch(this.cityURI + query)
+        const data = await response.json()
 
-    const response = await fetch(base + query)
-    const data = await response.json()
-
-    return data[0]
+        return data[0]
+    }
 }
